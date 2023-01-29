df2b1b569e2c7fd7b17d6f36fbf560abfb57eede
xia0hj <719887163@qq.com>
2023-01-30 00:21:40 +0800

    ci: update github action config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://xia0hj.github.io/my-notes/',
+  url: 'https://xia0hj.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
